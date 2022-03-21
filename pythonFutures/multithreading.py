8cde178b676c4ecf1572192084c5249491b683b8
vikram.sridhar <vikram.sridhar@toyotaconnected.co.in>
2022-03-21 11:20:35 +0530

    Futures

---
+++ pythonFutures/multithreading.py
@@ -0,0 +1,23 @@
+import concurrent.futures
+import time
+import requests
+import json
+
+def task(x):
+    #Assume this is the ec2 endpoint url. All predictions will happen in ec2s core
+    url="ec2endpointurl"
+    headers = {"content-type": "application/json"}
+    data=json.dumps({"input":x})
+    #Assume the model is deployed and an API is there to listen the incoming request in post entry point
+    prediction = requests.post(url, headers=headers, data=data)
+    return prediction
+
+#sameple inputs for prediction. Consider the length ogf list is 50.
+values=["Alexa switch on the tv","Hey alexa, play a song","Alexa call Anna"," ", " "]
+prediction=[]
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    #submit returns a future object. It is a acknowledgement about exectution of tasks in multithread
+    futures=[executor.submit(task,value) for value in values]
+    #as_completed is used to fetch the results as when each input is completed.
+    for future in as_completed(futures):
+        prediction.append(future.results())
